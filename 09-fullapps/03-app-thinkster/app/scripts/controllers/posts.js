063a9718e501e390f669f84574e938b090a90a48
jlamadrid <juan.lamadrid@gmail.com>
2014-07-22 16:51:36 -0400

    thinkster-app: posts add and delete

--- 09-fullapps/03-app-thinkster/app/scripts/controllers/posts.js
+++ 09-fullapps/03-app-thinkster/app/scripts/controllers/posts.js
@@ -9,4 +9,32 @@
  */
 app.controller('PostsCtrl', function ($scope) {
 
+    /**
+     * bound to form: e.g. <input type="text" ng-model="post.url" />
+     * as value in input text changes so do the attributes of this object
+     * @type {{url: string, title: string}}
+     */
+    $scope.post = {url: 'http://', title: ''};
+
+    /**
+     * array of post obbjects
+     * @type {Array}
+     */
+    $scope.posts = [];
+
+    /**
+     * function called by form submit
+     */
+    $scope.submitPost = function(){
+        //add post
+        $scope.posts.push($scope.post);
+
+        //reset post
+        $scope.post = {url: 'http://', title: ''};
+    }
+
+    $scope.deletePost = function(index){
+        $scope.posts.splice(index,1);
+    }
+
 });
